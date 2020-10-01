4e905336ed6ce994f4555e630e070ffc3fd1ab0e
Kaiguang Li <33639861+Kaiguang@users.noreply.github.com>
2020-10-01 12:12:15 -0600

    Remove test function

--- lambdaFunctions/shareHouseworkTestFunc/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = (event, context, callback) => {
-  const data = {
-    DYNAMODB_TABLE_NAME_CHORES: process.env.DYNAMODB_TABLE_NAME_CHORES,
-    DYNAMODB_TABLE_NAME_USERS: process.env.DYNAMODB_TABLE_NAME_USERS,
-  };
-
-  callback(null, data);
-};
